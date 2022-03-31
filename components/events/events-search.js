8363e7f87b6b6c2cc13f9c28ce011a05d5cbdf06
Dimitris Sapikas <d.sapikas@doubleip.com>
2022-03-31 22:44:14 +0300

    Done project

--- components/events/events-search.js
+++ components/events/events-search.js
@@ -1,18 +1,28 @@
+import { useRef } from "react";
 import Button from "../ui/button";
 import styles from './events-search.module.css';
 function EventSearch(props){
-    return <form className={styles.form}>
+    const yearInputRef = useRef();
+    const monthInputRef = useRef();
+
+    function submitHandler(event){
+        event.preventDefault();
+        const selectedYear = yearInputRef.current.value;
+        const selectedMonth = monthInputRef.current.value;
+        props.onSearch(selectedYear, selectedMonth);
+    }
+    return <form className={styles.form} onSubmit={submitHandler}>
         <div className={styles.controls}>
             <div className={styles.control}>
                 <label htmlFor="year">Year</label>
-                <select id='year'>
+                <select id='year' ref={yearInputRef}>
                     <option value='2021'>2021</option>
                     <option value='2022'>2022</option>
                 </select>
             </div>
             <div className={styles.control}>
                 <label htmlFor="month">Month</label>
-                <select id='month'>
+                <select id='month' ref={monthInputRef}>
                     <option id='1'>January</option>
                     <option id='2'>February</option>
                     <option id='3'>March</option>
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,14 +1,22 @@
 import { getAllEvents } from "../../dummy-data";
+import { useRouter } from "next/router";
 import EventList from '../../components/events/event-list';
 import EventSearch from "../../components/events/events-search";
 
 function EventsPage(){
+    const router = useRouter();
     const events = getAllEvents();
+
+    function findsEventHandler(year, month){
+        const fullpath = `/events/${year}/${month}`;
+
+        router.push(fullpath);
+    }
     return (
-        <div>
-            <EventSearch/>
+        <>
+            <EventSearch onSearch={findsEventHandler}/>
             <EventList items={events}/>
-        </div>
+        </>
     )
 }
 
